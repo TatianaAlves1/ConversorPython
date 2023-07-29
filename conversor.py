16865b8024e98cf8eebe1ffb9ddd91040f714504
TatianaAlves1 <tatipublisher@gmail.com>
2023-07-29 10:09:11 -0400

    Método de verificação da URL OK

--- conversor.py
+++ conversor.py
@@ -2,17 +2,29 @@ import requests
 from datetime import datetime
 
 class Conversor:
-    def __init__(self,url,valor) -> None:
-        self.__url = url
+    url = "http://economia.awesomeap.com.br/json/last/USD-BRL"
+
+    def __init__(self,valor) -> None:
         self.__valor = valor
         self.__data = datetime.now()
 
-    def obter_valor_dolar(self):
-        dolar = requests.get(self.__url).json()
-        print("valor atual: ",dolar)
+    @staticmethod
+    def verificar_disponibilidade():
+        try: 
+            cotacao = requests.get(Conversor.url).status_code
+            if cotacao == 200: 
+                return True
+            return False
+        except:
+            print("Ocorreu um erro ao acessar") 
+            return False
+        
+    def obter_valor_dolar(self) -> None:
+        cotacao = requests.get(self.__url).json()
+        dolar  = float(cotacao['USDBRL']['bid'])
+        print(f"Valor atual do dólar R$ {dolar:.2f}")
         print("Data atual: ", self.__data)
 
 
 
-teste = Conversor('http://economia.awesomeapi.com.br/json/last/USD-BRL',200)
-teste.obter_valor_dolar()
+print(Conversor.verificar_disponibilidade())
